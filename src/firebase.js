7e3109449184779a3e57c3c5f449feda09d98347
Swapnil <swapnil.vishu.7027@gmail.com>
2021-03-14 23:04:24 +0530

    added registration module

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,22 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
-import "firebase/storage";
 
-var config = {
-  apiKey: "AIzaSyCREQMYIbAYKscrS_JYvWP4KpMsF_jI5Bg",
-  authDomain: "react-slack-clone.firebaseapp.com",
-  databaseURL: "https://react-slack-clone.firebaseio.com",
-  projectId: "react-slack-clone",
-  storageBucket: "react-slack-clone.appspot.com",
-  messagingSenderId: "423395756405"
+import firebase from 'firebase/app';
+import "firebase/auth";
+ import "firebase/database";
+ import "firebase/storage";
+
+ var firebaseConfig = {
+  apiKey: "AIzaSyBOQ2dzGOjeaQl10bnIjMtuXf3yEJlJiJ8",
+  authDomain: "react-slack-chat-b4c68.firebaseapp.com",
+  projectId: "react-slack-chat-b4c68",
+  storageBucket: "react-slack-chat-b4c68.appspot.com",
+  messagingSenderId: "484714394410",
+  appId: "1:484714394410:web:57bc039811cbc48bcca453",
+  measurementId: "G-5VBGC2PDRL"
 };
-firebase.initializeApp(config);
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
 
 export default firebase;
